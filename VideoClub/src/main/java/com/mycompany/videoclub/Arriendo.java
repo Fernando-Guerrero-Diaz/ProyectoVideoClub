f2f6c4fbcd2495996f9a9f35e2614436dd5f0b21
Your Name <you@example.com>
2023-03-25 18:56:30 -0300

    Update Arriendo.java

--- VideoClub/src/main/java/com/mycompany/videoclub/Arriendo.java
+++ VideoClub/src/main/java/com/mycompany/videoclub/Arriendo.java
@@ -1,5 +1,55 @@
 package com.mycompany.videoclub;
+import java.time.LocalDate;
 
 public class Arriendo {
     private Película películaArrendada;
+    private LocalDate fechaArriendo;
+    private long díasArriendo;
+    
+    public Arriendo(Película película, long días){
+        películaArrendada = película;
+        fechaArriendo = LocalDate.now();
+        díasArriendo = días;
+    }
+    
+    public Película getPelícula(){
+        return películaArrendada;
+    }
+    
+    public void setPelícula(Película película){
+        películaArrendada = película;
+    }
+    
+    public LocalDate getFechaArriendo(){
+        return fechaArriendo;
+    }
+    
+    public void setFechaArriendo(LocalDate fecha){
+        fechaArriendo = fecha;
+    }
+    
+    public LocalDate getFechaDevolución(){
+        return fechaArriendo.plusDays(díasArriendo);
+    }
+    
+    public long getDíasArriendo(){
+        return díasArriendo;
+    }
+    
+    public void setDíasArriendo(long días){
+        díasArriendo = días;
+    }
+    
+    public boolean Atrasado(){
+        LocalDate díaHoy = LocalDate.now();
+        int diferenciaDías = díaHoy.compareTo(fechaArriendo.plusDays(díasArriendo));
+        return diferenciaDías > 0;
+    }
+    
+    public int DíasDeAtraso(){
+        LocalDate díaHoy = LocalDate.now();
+        int diferenciaDías = díaHoy.compareTo(fechaArriendo.plusDays(díasArriendo));
+        if (diferenciaDías < 0) return 0;
+        return diferenciaDías;
+    }
 }
