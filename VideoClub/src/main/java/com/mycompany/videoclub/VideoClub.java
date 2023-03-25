1de4c26eda71e1cba175ac6565a3ff01b61e2c83
NicoTorfan <111155302+NicoTorfan@users.noreply.github.com>
2023-03-25 19:07:59 -0300

    Update VideoClub.java

--- VideoClub/src/main/java/com/mycompany/videoclub/VideoClub.java
+++ VideoClub/src/main/java/com/mycompany/videoclub/VideoClub.java
@@ -15,13 +15,25 @@ public class VideoClub {
         float Pun= Float.parseFloat(MM.readLine());
         System.out.println("Ingrese Cantidad de Peliculas en la tienda: ");
         int Sto= Integer.parseInt(MM.readLine());
-        //System.out.println("Ingrese Fecha de inicio de Arriendo de la película: ");
-        //int Arr= Integer.parseInt(MM.readLine());
+        System.out.println("Ingrese Fecha de inicio de Arriendo de la película: ");
+        int Arr= Integer.parseInt(MM.readLine());
         System.out.println("Ingrese precio del arriendo de la pélicula: ");
         int Pre= Integer.parseInt(MM.readLine());
-        System.out.println(Pel + Dir + Pun + Sto + Pre);
-        
-        //String[] Generos= new String[3];
+        String[] generos= new String[3];
+        String[] actores= new String[3];
+        for (int i=0;i<3;i++){
+            System.out.println("Ingrese generos de la pélicula(" + (i+1) + "/3)");
+            generos[i]=MM.readLine();
+        }
+        for (int i=0;i<3;i++){
+            System.out.println("Ingrese Actores Principales de la pélicula(" + (i+1) + "/3)");
+            actores[i]=MM.readLine();
+        }
+        Película nuevaPeli = new Película(Pel,Dir,Pun,Sto,Arr,Pre);
+        nuevaPeli.setGéneros(generos);
+        nuevaPeli.setActoresPrincipales(actores);
+        System.out.println(nuevaPeli.getNombre() + " " + nuevaPeli.getDirector()+ " " + nuevaPeli.getPuntuación() + " " + nuevaPeli.getStock()+ " ");
+        System.out.println(nuevaPeli.getArriendos()+ " " + nuevaPeli.getPrecioArriendo());
         //Generos[0]="Rock";
         //Pelicula2.setGéneros(Generos);
         //System.out.println(Pelicula2.getNombre()+ Pelicula2.getStock());
