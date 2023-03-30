2013cfc2fee199d9dcad0ae881a3acf48c99f8c9
freksel <alejandro.marin.s@mail.pucv.cl>
2023-03-30 20:05:42 -0300

    Nueva Opción Menu Arriendo
    Se agrega el 3er caso para registrar 1 nuevo Arriendo de película.

--- VideoClub/src/main/java/com/mycompany/videoclub/Menu.java
+++ VideoClub/src/main/java/com/mycompany/videoclub/Menu.java
@@ -12,11 +12,11 @@ public Menu(int opción){
     this.opción = opción;
 }
 public static void Menu2(HashMap<Integer,Cliente> map) throws IOException {
-    System.out.println("¡Bienvenido a la tienda de Películas!\n");
+    System.out.println("¡Bienvenido a la tienda de Películas!");
     BufferedReader MM=new BufferedReader (new InputStreamReader(System.in));
     boolean running = true;
     while(running){
-        System.out.println("Escoge una opción: \n 1-Agregar una película  en el catalogo. \n 2-Agregar nuevo Cliente en el sistema. \n 3-Terminar Programa.");
+        System.out.println("Escoge una opción: \n 1-Agregar una película  en el catalogo. \n 2-Agregar nuevo Cliente en el sistema. \n 3-Agregar nuevo arriendo \n 4-Terminar Programa.");
         int menu= Integer.parseInt(MM.readLine());
         switch (menu){
             case 1:
@@ -52,7 +52,7 @@ public static void Menu2(HashMap<Integer,Cliente> map) throws IOException {
                 System.out.println("Ingrese nombre del nuevo usuario: ");
                 //Pelicula2.setNombre(MM.readLine());
                 String Cli=MM.readLine();
-                System.out.println("Ingrese Rut del nuevo usuario: ");
+                System.out.println("Ingrese Rut del nuevo usuario sin puntos ni número verificador: ");
                 int Rut= Integer.parseInt(MM.readLine());
                 if(map.containsKey(Rut)){
                     System.out.println("No se puede crear un usuario ya existente");
@@ -64,16 +64,37 @@ public static void Menu2(HashMap<Integer,Cliente> map) throws IOException {
                 nuevoCli.print();
                 //HashMap<Integer,Cliente> map = new HashMap();
                 map.put(nuevoCli.getRut(),nuevoCli);
-            if(map.containsKey(nuevoCli.getRut())){
-                map.get(nuevoCli.getRut()).print();
-            }
+                //if(map.containsKey(nuevoCli.getRut())){
+                //map.get(nuevoCli.getRut()).print();
+            //}
             break;
+            case 3:
+                System.out.println("Ingrese Rut del Cliente para comenzar el arriendo: ");
+                int Ru=Integer.parseInt(MM.readLine());
+                if(map.containsKey(Ru)){
+                    System.out.println("Ingrese Nombre de la película para arrendar: ");
+                    String Peli=MM.readLine();
+                    System.out.println("Ingrese cuantos días van a arrendar la película: ");
+                    int Dia=Integer.parseInt(MM.readLine());
+                    System.out.println("Ingrese el precio del arriendo: ");
+                    int Pre=Integer.parseInt(MM.readLine());
+                    Arriendo nuevoArr= new Arriendo(Peli,Dia,Pre);
+                    nuevoArr.print();
+                    Cliente cc =map.get(Ru);
+                    cc.AddFirstArriendo(nuevoArr);
+                }
+                
+                else{
+                  System.out.println("El usuario no existe.");
+                }
                 //Generos[0]="Rock";
                 //Pelicula2.setGéneros(Generos);
                 //System.out.println(Pelicula2.getNombre()+ Pelicula2.getStock());
-            case 3:
+                break;
+            case 4:
                 running=false;
                 break;
+            case 5:
             default:
                 System.out.println("Solo escoger entre 1 y 2 ");
                 break;
